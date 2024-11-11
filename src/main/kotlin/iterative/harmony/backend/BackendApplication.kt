ed8a71b778f70bff435cffbde77e9fdc9650f48b
AptFox <AptFox@users.noreply.github.com>
2024-11-11 17:41:26 -0500

    initial commit

---
+++ src/main/kotlin/iterative/harmony/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package iterative.harmony.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendApplication>(*args)
+}
