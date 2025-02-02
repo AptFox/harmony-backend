aa2bd0c6b5352549498a28b637c0d180533d6f2c
AptFox <AptFox@users.noreply.github.com>
2025-02-01 19:34:22 -0500

    linting

--- src/test/kotlin/iterative/harmony/backend/service/UserServiceTest.kt
+++ src/test/kotlin/iterative/harmony/backend/service/UserServiceTest.kt
@@ -9,11 +9,11 @@ import iterative.harmony.backend.model.User
 import iterative.harmony.backend.repository.RoleRepository
 import iterative.harmony.backend.repository.UserRepository
 import iterative.harmony.backend.util.RoleConstants
-import org.junit.jupiter.api.Test
-
+import java.util.*
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.ArgumentCaptor
 import org.mockito.InjectMocks
@@ -21,35 +21,22 @@ import org.mockito.Mock
 import org.mockito.Mockito.*
 import org.mockito.Mockito.`when` as whenever
 import org.mockito.junit.jupiter.MockitoExtension
-import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.core.Authentication
-import java.util.*
-
+import org.springframework.security.core.context.SecurityContext
 
 @ExtendWith(MockitoExtension::class)
 class UserServiceTest {
 
-    @Mock
-    private lateinit var userRepository: UserRepository
+    @Mock private lateinit var userRepository: UserRepository
 
-    @Mock
-    private lateinit var roleRepository: RoleRepository
+    @Mock private lateinit var roleRepository: RoleRepository
 
-    @InjectMocks
-    private lateinit var userService: UserService
+    @InjectMocks private lateinit var userService: UserService
 
     private val uuid: UUID = UUID.fromString("306420e2-5f30-4070-a5c1-b9961bf10ef4")
     private val discordUser = DiscordOAuthUser("1", "username", "globalName")
     private val userRole = Role(1, RoleConstants.USER_ROLE, "The default role for a user")
-    private val expectedUser = User(
-        uuid,
-        "username",
-        "username",
-        "1",
-        0,
-        setOf(userRole)
-    )
-
+    private val expectedUser = User(uuid, "username", "username", "1", 0, setOf(userRole))
 
     @Nested
     @DisplayName("getOrCreateUser")
@@ -61,8 +48,10 @@ class UserServiceTest {
 
             @Test
             fun `should create and return new user`() {
-                whenever(userRepository.findByDiscordId(discordUser.id)).thenReturn(Optional.empty())
-                whenever(roleRepository.findByName(RoleConstants.USER_ROLE)).thenReturn(Optional.of(userRole))
+                whenever(userRepository.findByDiscordId(discordUser.id))
+                    .thenReturn(Optional.empty())
+                whenever(roleRepository.findByName(RoleConstants.USER_ROLE))
+                    .thenReturn(Optional.of(userRole))
                 whenever(userRepository.save(any())).thenReturn(expectedUser)
 
                 val argumentCaptor: ArgumentCaptor<User> = ArgumentCaptor.captor()
@@ -85,7 +74,8 @@ class UserServiceTest {
         inner class PreExistingUser() {
             @Test
             fun `should return existing user`() {
-                whenever(userRepository.findByDiscordId(discordUser.id)).thenReturn(Optional.of(expectedUser))
+                whenever(userRepository.findByDiscordId(discordUser.id))
+                    .thenReturn(Optional.of(expectedUser))
 
                 verify(userRepository, never()).save(any())
 
@@ -112,7 +102,8 @@ class UserServiceTest {
             // assert that the expected userResponse is returned
 
             val actualUserResponse = userService.getCurrentUser(securityContext)
-            val expectedUserResponse = UserResponse(uuid, expectedUser.displayName, expectedUser.timeZoneId)
+            val expectedUserResponse =
+                UserResponse(uuid, expectedUser.displayName, expectedUser.timeZoneId)
 
             assertEquals(expectedUserResponse, actualUserResponse)
         }
@@ -129,18 +120,17 @@ class UserServiceTest {
         inner class PreExistingUser() {
             @Test
             fun `should update the user`() {
-                val updatedUser = expectedUser.apply {
-                    displayName = updateUserRequest.displayName
-                    timeZoneId = updateUserRequest.timeZoneId.toInt()
-                }
+                val updatedUser =
+                    expectedUser.apply {
+                        displayName = updateUserRequest.displayName
+                        timeZoneId = updateUserRequest.timeZoneId.toInt()
+                    }
 
-                whenever(userRepository.findByUsername(expectedUser.username)).thenReturn(Optional.of(expectedUser))
+                whenever(userRepository.findByUsername(expectedUser.username))
+                    .thenReturn(Optional.of(expectedUser))
                 whenever(userRepository.save(updatedUser)).thenReturn(updatedUser)
 
-                val actual = userService.updateUser(
-                    updateUserRequest,
-                    expectedUser.username
-                )
+                val actual = userService.updateUser(updateUserRequest, expectedUser.username)
 
                 assertEquals(updateUserRequest.displayName, actual.displayName)
                 assertEquals(updateUserRequest.timeZoneId.toInt(), actual.timeZoneId)
@@ -152,13 +142,11 @@ class UserServiceTest {
         inner class NonExistingUser() {
             @Test
             fun `should throw UserNotFoundException`() {
-                whenever(userRepository.findByUsername(expectedUser.username)).thenReturn(Optional.empty())
+                whenever(userRepository.findByUsername(expectedUser.username))
+                    .thenReturn(Optional.empty())
 
                 assertThrows(UserNotFoundException::class.java) {
-                    userService.updateUser(
-                        updateUserRequest,
-                        expectedUser.username
-                    )
+                    userService.updateUser(updateUserRequest, expectedUser.username)
                 }
             }
         }
