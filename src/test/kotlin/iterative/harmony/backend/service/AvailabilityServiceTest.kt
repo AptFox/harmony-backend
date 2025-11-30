bb675cfd23d3a24ba52a32652b277d41475a5f88
AptFox <AptFox@users.noreply.github.com>
2025-11-30 12:18:12 +0100

    finished adding availability tests

--- src/test/kotlin/iterative/harmony/backend/service/AvailabilityServiceTest.kt
+++ src/test/kotlin/iterative/harmony/backend/service/AvailabilityServiceTest.kt
@@ -1,11 +1,13 @@
 package iterative.harmony.backend.service
 
+import iterative.harmony.backend.controller.dto.AvailabilityExceptionRequest
 import iterative.harmony.backend.controller.dto.AvailabilityResponse
 import iterative.harmony.backend.controller.dto.WeeklyAvailabilitySlotRequest
 import iterative.harmony.backend.model.AvailabilityException
 import iterative.harmony.backend.model.WeeklyAvailabilitySlot
 import iterative.harmony.backend.repository.AvailabilityExceptionRepository
 import iterative.harmony.backend.repository.WeeklyAvailabilitySlotRepository
+import iterative.harmony.backend.util.AvailabilityConstants.DAYS_OF_WEEK
 import java.time.Duration
 import java.time.Instant
 import java.time.LocalTime
@@ -18,6 +20,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito.verify
+import org.mockito.internal.verification.Times
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.any
 import org.mockito.kotlin.doNothing
@@ -173,16 +176,106 @@ class AvailabilityServiceTest {
         inner class OverlappingSlots() {
             @Test
             fun `slots are merged, saved and returned`() {
-                throw NotImplementedError()
+                val timeZoneId = "America/New_York"
+                val expectedMergedStartTime = LocalTime.NOON
+                val expectedMergedEndTime = LocalTime.NOON.plus(Duration.ofHours(6))
+                val requests: MutableList<WeeklyAvailabilitySlotRequest> =
+                    mutableListOf<WeeklyAvailabilitySlotRequest>()
+                for (i in 1..6) {
+                    requests.add(
+                        WeeklyAvailabilitySlotRequest(
+                            dayOfWeek,
+                            startTime = expectedMergedStartTime,
+                            endTime = LocalTime.NOON.plus(Duration.ofHours(i.toLong())),
+                            timeZoneId,
+                        )
+                    )
+                }
+                val expectedSlot =
+                    WeeklyAvailabilitySlot(
+                        userId = userId,
+                        dayOfWeek = dayOfWeek,
+                        startTime = expectedMergedStartTime,
+                        endTime = expectedMergedEndTime,
+                        timeZoneId = timeZoneId,
+                    )
+                val expectedMergedSlots = mutableListOf(expectedSlot)
+
+                val expected =
+                    WeeklyAvailabilitySlot(
+                        id = 1L,
+                        userId,
+                        2L,
+                        dayOfWeek,
+                        startTime = expectedMergedStartTime,
+                        endTime = expectedMergedEndTime,
+                        timeZoneId = "America/New_York",
+                    )
+
+                whenever(weeklyAvailabilitySlotRepositoryMock.saveAll(expectedMergedSlots))
+                    .thenReturn(mutableListOf(expected))
+                whenever(weeklyAvailabilitySlotRepositoryMock.deleteAllByUserId(userId))
+                    .thenReturn(null)
+
+                val response =
+                    availabilityService.overwriteWeeklyAvailability(userId.toString(), requests)
+                val actual = response.weeklyAvailabilitySlots?.get(0)
+                assertEquals(expected, actual)
+                verify(weeklyAvailabilitySlotRepositoryMock).saveAll(expectedMergedSlots)
+                verify(weeklyAvailabilitySlotRepositoryMock).deleteAllByUserId(userId)
             }
         }
 
         @Nested
-        @DisplayName("when called with valid slots")
+        @DisplayName("when called with non-overlapping slots")
         inner class ValidSlots() {
             @Test
-            fun `slots are saved and returned`() {
-                throw NotImplementedError()
+            fun `separate slots are saved and returned`() {
+                val timeZoneId = "America/New_York"
+                val expectedMergedStartTime = LocalTime.NOON
+                val expectedMergedEndTime = LocalTime.NOON.plus(Duration.ofHours(6))
+                val requests: MutableList<WeeklyAvailabilitySlotRequest> =
+                    mutableListOf<WeeklyAvailabilitySlotRequest>()
+                for (i in 0..6) {
+                    requests.add(
+                        WeeklyAvailabilitySlotRequest(
+                            DAYS_OF_WEEK.elementAt(i),
+                            startTime = expectedMergedStartTime,
+                            endTime = expectedMergedEndTime,
+                            timeZoneId,
+                        )
+                    )
+                }
+
+                val expectedUnmergedSlots: MutableList<WeeklyAvailabilitySlot> = mutableListOf()
+
+                for (i in 0..6) {
+                    expectedUnmergedSlots.add(
+                        WeeklyAvailabilitySlot(
+                            userId = userId,
+                            dayOfWeek = DAYS_OF_WEEK.elementAt(i),
+                            startTime = expectedMergedStartTime,
+                            endTime = expectedMergedEndTime,
+                            timeZoneId = timeZoneId,
+                        )
+                    )
+                }
+
+                whenever(weeklyAvailabilitySlotRepositoryMock.saveAll(expectedUnmergedSlots))
+                    .thenReturn(expectedUnmergedSlots)
+                whenever(weeklyAvailabilitySlotRepositoryMock.deleteAllByUserId(userId))
+                    .thenReturn(null)
+
+                val response =
+                    availabilityService.overwriteWeeklyAvailability(userId.toString(), requests)
+                val actual = response.weeklyAvailabilitySlots
+                for (i in 0..6) {
+                    val actualSlot = actual?.get(i)
+                    val expectedSlot = expectedUnmergedSlots[i]
+                    assertEquals(expectedSlot, actualSlot)
+                }
+                verify(weeklyAvailabilitySlotRepositoryMock).saveAll(expectedUnmergedSlots)
+                verify(weeklyAvailabilitySlotRepositoryMock).deleteAllByUserId(userId)
             }
         }
     }
@@ -193,44 +286,127 @@ class AvailabilityServiceTest {
         @Nested
         @DisplayName("when called with invalid AvailabilityExceptions")
         inner class InvalidExceptions() {
-            @Test
-            fun `when times are invalid format should return error`() {
-                throw NotImplementedError()
-            }
 
-            @Test
-            fun `when comment is invalid should return error`() {
-                throw NotImplementedError()
+            fun generateExceptionRequest(
+                amountToAdd: Duration,
+                minus: Boolean = false,
+            ): AvailabilityExceptionRequest {
+                val expectedStartTime = Instant.now()
+                val expectedEndTime =
+                    if (minus) {
+                        expectedStartTime.minus(amountToAdd)
+                    } else {
+                        expectedStartTime.plus(amountToAdd)
+                    }
+
+                return AvailabilityExceptionRequest(
+                    startTime = expectedStartTime,
+                    endTime = expectedEndTime,
+                    comment = null,
+                )
             }
 
             @Test
             fun `when startTime and endTime are the same should return error`() {
-                throw NotImplementedError()
+                val request = generateExceptionRequest(Duration.ofHours(0))
+
+                whenever(
+                        availabilityExceptionRepositoryMock.existsByUserIdAndStartTimeEquals(
+                            userId,
+                            request.startTime,
+                        )
+                    )
+                    .thenReturn(false)
+
+                val response =
+                    availabilityService.addAvailabilityException(userId.toString(), request)
+                val expected =
+                    "[startTime and endTime are the same, availability changes must be >=60 min]"
+                val actual = response.errors
+                assertEquals(expected, actual)
+                verify(availabilityExceptionRepositoryMock, Times(0)).save(any())
             }
 
             @Test
             fun `when endTime is before startTime should return error`() {
-                throw NotImplementedError()
+                val request = generateExceptionRequest(Duration.ofHours(1), true)
+
+                whenever(
+                        availabilityExceptionRepositoryMock.existsByUserIdAndStartTimeEquals(
+                            userId,
+                            request.startTime,
+                        )
+                    )
+                    .thenReturn(false)
+
+                val response =
+                    availabilityService.addAvailabilityException(userId.toString(), request)
+                val expected =
+                    "[endTime is before startTime, availability changes must be >=60 min]"
+                val actual = response.errors
+                assertEquals(expected, actual)
+                verify(availabilityExceptionRepositoryMock, Times(0)).save(any())
             }
 
             @Test
             fun `when exception is more than 24 hours should return error`() {
-                throw NotImplementedError()
+                val request = generateExceptionRequest(Duration.ofHours(25))
+
+                whenever(
+                        availabilityExceptionRepositoryMock.existsByUserIdAndStartTimeEquals(
+                            userId,
+                            request.startTime,
+                        )
+                    )
+                    .thenReturn(false)
+
+                val response =
+                    availabilityService.addAvailabilityException(userId.toString(), request)
+                val expected = "[availability changes must be <= 24 hours]"
+                val actual = response.errors
+                assertEquals(expected, actual)
+                verify(availabilityExceptionRepositoryMock, Times(0)).save(any())
             }
 
             @Test
             fun `when exception is less than 1 hour should return error`() {
-                throw NotImplementedError()
-            }
+                val request = generateExceptionRequest(Duration.ofMinutes(6))
 
-            @Test
-            fun `when exception is in the past should return error`() {
-                throw NotImplementedError()
+                whenever(
+                        availabilityExceptionRepositoryMock.existsByUserIdAndStartTimeEquals(
+                            userId,
+                            request.startTime,
+                        )
+                    )
+                    .thenReturn(false)
+
+                val response =
+                    availabilityService.addAvailabilityException(userId.toString(), request)
+                val expected = "[availability changes must be >=60 min]"
+                val actual = response.errors
+                assertEquals(expected, actual)
+                verify(availabilityExceptionRepositoryMock, Times(0)).save(any())
             }
 
             @Test
             fun `when exception is more than 3 months in the future should return error`() {
-                throw NotImplementedError()
+                val request = generateExceptionRequest(Duration.ofDays(91))
+
+                whenever(
+                        availabilityExceptionRepositoryMock.existsByUserIdAndStartTimeEquals(
+                            userId,
+                            request.startTime,
+                        )
+                    )
+                    .thenReturn(false)
+
+                val response =
+                    availabilityService.addAvailabilityException(userId.toString(), request)
+                val expected =
+                    "[availability changes must be <= 24 hours, availability exceptions must be within 90 days]"
+                val actual = response.errors
+                assertEquals(expected, actual)
+                verify(availabilityExceptionRepositoryMock, Times(0)).save(any())
             }
         }
 
@@ -239,7 +415,37 @@ class AvailabilityServiceTest {
         inner class ValidExceptions() {
             @Test
             fun `expired exceptions are deleted and new exceptions are saved and returned`() {
-                throw NotImplementedError()
+                val expectedStartTime = Instant.now()
+                val expectedEndTime = expectedStartTime.plus(Duration.ofHours(4))
+
+                val request =
+                    AvailabilityExceptionRequest(
+                        startTime = expectedStartTime,
+                        endTime = expectedEndTime,
+                        comment = null,
+                    )
+                val expected =
+                    AvailabilityException(
+                        id = null,
+                        userId = userId,
+                        playerId = null,
+                        startTime = request.startTime,
+                        endTime = request.endTime,
+                        comment = null,
+                    )
+                whenever(availabilityExceptionRepositoryMock.save(expected)).thenReturn(expected)
+                whenever(
+                        availabilityExceptionRepositoryMock.existsByUserIdAndStartTimeEquals(
+                            userId,
+                            request.startTime,
+                        )
+                    )
+                    .thenReturn(false)
+
+                val response =
+                    availabilityService.addAvailabilityException(userId.toString(), request)
+                val actual = response.exceptions
+                assertEquals(expected, actual)
             }
         }
     }
