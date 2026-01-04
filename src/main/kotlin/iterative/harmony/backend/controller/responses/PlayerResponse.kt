413b3dd65d1b21fba55c6cdc196e88137ab5629e
AptFox <AptFox@users.noreply.github.com>
2026-01-04 13:07:48 +0700

    Added org to playerResponse

--- src/main/kotlin/iterative/harmony/backend/controller/responses/PlayerResponse.kt
+++ src/main/kotlin/iterative/harmony/backend/controller/responses/PlayerResponse.kt
@@ -5,6 +5,7 @@ import java.util.UUID
 data class PlayerResponse(
     var id: Long,
     var name: String,
+    var organization: OrganizationResponse,
     var team: TeamResponse?,
     var userId: UUID,
     var teamRole: String?,
